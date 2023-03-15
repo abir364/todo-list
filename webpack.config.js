6ce02049438ce5087999875489ae6092827b9348
abir364 <abirh364@gmail.com>
2023-03-16 01:50:33 +0600

    generated boilerplate code set up for webpack and tailwind

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+  },
+  output: {
+    filename: 'main.js',
+    assetModuleFilename: 'src/assets/[name][ext]',
+    path: path.resolve(__dirname, 'dist'),
+    // clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
